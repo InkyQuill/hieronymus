d1862afef44bdfad25eed5090bcf81a33340aa0e
Inky Quill <me@inkyquill.net>
2026-07-17 11:56:41 +0300

    feat: wire modular CSS and default dark theme

--- frontend/src/web/main.ts
+++ frontend/src/web/main.ts
@@ -1,5 +1,8 @@
 import { mount } from "svelte";
 import App from "./App.svelte";
-import "./styles.css";
+import "./fonts.css";
+import "./tokens.css";
+import "./base.css";
+import "./components.css";
 
 mount(App, { target: document.getElementById("app")! });
